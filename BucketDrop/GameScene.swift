020562649bbcee52e61de3918542b141ee1c8b35
Eastae <eastman.ae@gmail.com>
2020-12-10 18:34:54 -0800

    implemented core game logic collision finalized, still so much to do

--- BucketDrop/GameScene.swift
+++ BucketDrop/GameScene.swift
@@ -9,81 +9,164 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    let dropCategory: UInt32 = 1 << 0
+    let bucketCategory: UInt32 = 1 << 1
+    var scoreCounter: Int = 0
+    //Variables and parameters
+    
+    
+    
+    //Hello World node (optional)
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var contentCreated = false
+    
+    //HUD elements
+    let daScoreBoard = "scoreHud"
+    
+    //objects
+    var playerBucket : SKSpriteNode!
+    var background : SKEmitterNode!
+    var scoreLabel : SKLabelNode!
+    
+    func setupHud(){
+        let scoreLabel = SKLabelNode(fontNamed: "Courier")
+        scoreLabel.name = daScoreBoard
+        scoreLabel.fontSize = 25
+
+        // 2
+        scoreLabel.fontColor = SKColor.white
+        scoreLabel.text = String(format: "Score: %04u", 0)
+
+        scoreLabel.position = CGPoint(
+          x: frame.size.width / 2,
+          y: size.height - 50
+        )
+
+
+        addChild(scoreLabel)
+    }
+
     
     override func didMove(to view: SKView) {
+        //non-gravity based movement
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        //TODO increment scoreboard
+        setupHud()
+        
+        //initialize background rainfall emitter
+        background = SKEmitterNode (fileNamed: "Rain.sks")
+        background.position = CGPoint(x: size.width/2, y: size.height)
+        self.addChild(background)
+        
+
+        //our brave hero!
+        playerBucket = SKSpriteNode(imageNamed: "bucketE.png")
+        playerBucket.position = CGPoint(x: size.width/2, y: 100)
+        //Bind Physical properties
+        playerBucket.physicsBody = SKPhysicsBody(rectangleOf: playerBucket.size)
+        playerBucket.physicsBody?.isDynamic = false
+        playerBucket.physicsBody?.categoryBitMask = bucketCategory
+        playerBucket.physicsBody?.contactTestBitMask = dropCategory
+        addChild(playerBucket)
+        
+        
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        view.addGestureRecognizer(recognizer)
+        
+        run(SKAction.repeatForever(
+          SKAction.sequence([
+            SKAction.run(addDroplet),
+            SKAction.wait(forDuration: 1.0)
+            ])
+        ))
+//      Get label node from scene and store it for use later
+        self.label = self.childNode(withName: "//helloWorld") as? SKLabelNode
         if let label = self.label {
+            label.text = "Bucket Drop"
             label.alpha = 0.0
+            label.position = CGPoint(x: size.width/2, y: size.height*0.9)
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func random() -> CGFloat {
+    //get a potentially huge random varaible
+      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    //normalize the random variable
+      return random() * (max - min) + min
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    func addDroplet(){
+        let drop = SKSpriteNode(imageNamed: "drop.png")
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //the watery menace!
+        drop.physicsBody = SKPhysicsBody(circleOfRadius:  drop.size.width/2)
+        //bind physical properties
+        drop.physicsBody?.isDynamic = true
+        drop.physicsBody?.categoryBitMask = dropCategory
+        drop.physicsBody?.collisionBitMask = bucketCategory
+        drop.physicsBody?.contactTestBitMask = bucketCategory
+        drop.physicsBody?.usesPreciseCollisionDetection = true
+        
+        
+        
+        
+        let actualX = random(min: drop.size.width/2, max: size.width - drop.size.width/2)
+        
+        drop.position = CGPoint(x : actualX, y: size.height - drop.size.height/2)
+        
+        addChild(drop)
+        //how fast the drops move
+        let dropTime = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        let actionMove = SKAction.move(to: CGPoint(x: actualX, y : -drop.size.width/2), duration: TimeInterval(dropTime))
+        let actionMoveDone = SKAction.removeFromParent()
+        drop.run(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+
+    
+    
+    @objc func tap(recognizer: UIGestureRecognizer){
+        let playerSpeed = CGFloat(700)
+        
+        let viewLocation = recognizer.location(in: view)
+        let sceneLocation = convertPoint(fromView: viewLocation)
+        let gap = xDistance(a: sceneLocation, b: playerBucket.position)
+        let realTime = travelTimeForDist(distance: gap, speed: playerSpeed)
+        let moveToAction = SKAction.moveTo(x: sceneLocation.x, duration: realTime)
+        playerBucket.run(moveToAction)
+    }
+    //calculate x distance
+    func xDistance (a: CGPoint, b: CGPoint) -> CGFloat {
+        return sqrt(pow((a.x - b.x), 2.0))
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    
+    func travelTimeForDist(distance: CGFloat, speed: CGFloat) -> TimeInterval{
+        let time = distance/speed
+        return TimeInterval(time)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        //TODO implement filled bucket, fix score counter
+        if bodyA.categoryBitMask == dropCategory{
+            bodyA.node?.removeFromParent()
+        } else if bodyB.categoryBitMask == dropCategory{
+            bodyB.node?.removeFromParent()
+        }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
